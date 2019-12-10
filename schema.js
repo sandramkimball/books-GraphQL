2b0159cdb42fdb9d8eed6a14aa4a499808d7e598
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-12-10 09:08:15 -0800

    agg. api with google trnslt

--- schema.js
+++ schema.js
@@ -3,18 +3,41 @@ const util = require('util')
 const parseXML = util.promisify(require('xml2js').parseString)
 const {GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLList, GraphQLString} = require('graphql')
 
-// fetch('https://www.goodreads.com/author/show.xml?id=4432&key=risKm8wwXsIcyEiTktvA'
-// )
-// .then(res=> res.text())
-// .then(parseXML)
+//Functions
+function translate(lang, str) {
+    // Google Translate API is a paid (but dirt cheap) service. This is my key
+    // and will be disabled by the time the video is out. To generate your own,
+    // go here: https://cloud.google.com/translate/v2/getting_started
+    const apiKey =
+      'AIzaSyBN-bwtos8sKU6X84wkrdjtCF7uzng6kgQ'
+      const url =
+      'https://www.googleapis.com' +
+      '/language/translate/v2' +
+        '?key=' + apiKey +
+      '&source=en' +
+      '&target=' + lang +
+      '&q=' + encodeURIComponent(str)
+    return fetch(url)
+           .then(response => response.json())
+        .then(parsedResponse =>
+          parsedResponse
+            .data
+          .translations[0]
+          .translatedText
+      )
+  }
 
+  //Object Types
 const BookType = new GraphQLObjectType({
     name: 'Book',
     description: '...',
     fields: ()=>({
         title: {
             type: GraphQLString,
-            resolve: xml => xml.title[0]
+            resolve: (xml, args) => {
+            const title = xml.GoodreadsResponse.book[0].title[0]
+            return ars.lang ? translate(args.lang, title) : title
+            }
         },
         isbn: {
             type: GraphQLString,
@@ -36,8 +59,9 @@ const AuthorType = new GraphQLObjectType({
             resolve: xml => {
                 //this grabs data from parent element and extracts the ids.
                 const ids = xml.GoodreadsResponse.author[0].books[0].book.map(elem=> elem.id[0]._)
-                return Promise.all(ids.map(id=> 
-                    //then for each id extract the book info
+                console.log('Fetching Books!')
+                    return Promise.all(ids.map(id=> 
+                    //then for each id extract the book info and eventually passed into each BookType
                     fetch(`https://www.goodreads.com/book/show${id}.xml?key=42tmzmwXfJJzHcbXlBRg`)
                     .then(res=> res.text())
                     .then(parseXML)
