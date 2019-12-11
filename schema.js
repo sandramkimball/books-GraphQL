ca300f2953496250585352e357873902308e4d40
sandramkimball <53502963+sandramkimball@users.noreply.github.com>
2019-12-10 20:57:30 -0800

    transfered funcs to server.js

--- schema.js
+++ schema.js
@@ -1,64 +1,41 @@
-// const fetch = require('node-fetch')
-const util = require('util')
-const parseXML = util.promisify(require('xml2js').parseString)
-const {GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLList, GraphQLString} = require('graphql')
+//SCHEMA
+//Contains types and all info on HOW to get data.
+//Where the magic happens.
+const {
+    GraphQLSchema, 
+    GraphQLObjectType, 
+    GraphQLInt, 
+    GraphQLList, 
+    GraphQLString
+    } = require('graphql')
 
-//Functions
-function translate(lang, str) {
-    // Google Translate API is a paid (but dirt cheap) service. This is my key
-    // and will be disabled by the time the video is out. To generate your own,
-    // go here: https://cloud.google.com/translate/v2/getting_started
-    const apiKey =
-      'AIzaSyBN-bwtos8sKU6X84wkrdjtCF7uzng6kgQ'
-      const url =
-      'https://www.googleapis.com' +
-      '/language/translate/v2' +
-        '?key=' + apiKey +
-      '&source=en' +
-      '&target=' + lang +
-      '&q=' + encodeURIComponent(str)
-    return fetch(url)
-           .then(response => response.json())
-        .then(parsedResponse =>
-          parsedResponse
-            .data
-          .translations[0]
-          .translatedText
-      )
-  }
-
-const fetchAuthor = id => {
-    fetch(`https://www.goodreads.com/author/show.xml?id=${id}&key=x7VoLKspIa5LO9bMmwxvIw`
-    )
-    .then(res=> res.text())
-    .then(parseXML)
-}
-
-  //Object Types
+//Object Types
 const BookType = new GraphQLObjectType({
     name: 'Book',
     description: '...',
+
     fields: ()=>({
         title: {
             type: GraphQLString,
+            args: {
+                lang: {type: GraphQLString}
+            },
             resolve: (xml, args) => {
             const title = xml.GoodreadsResponse.book[0].title[0]
             return ars.lang ? translate(args.lang, title) : title
             }
         },
-
         isbn: {
             type: GraphQLString,
-            resolve: xml=>xml.isbn[0]
+            resolve: xml=>xml.GoodreadsResponse.book[0].isbn[0]
         },
 
         authors: {
             type: new GraphQLList(AuthorType),
-            resolve: xml=>{
-            //returns array of strings
-            const authorElements= xml.GoodreadsResponse.book[0].authors[0].author
-            const ids = authorElements.map(elem=> elem.id[0])
-            return Promise.all(ids.map(fetchAuthor))
+            resolve: (xml, args, contect)=>{
+                const authorElements= xml.GoodreadsResponse.book[0].authors[0].author
+                const ids = authorElements.map(elem=> elem.id[0])
+                return context.authorLoader.loadMany(ids)
         }}
     })
 });
@@ -66,6 +43,7 @@ const BookType = new GraphQLObjectType({
 const AuthorType = new GraphQLObjectType({
     name: 'Author',
     description: '...',
+
     fields: ()=> ({
         name: {
             type: GraphQLString,
@@ -73,18 +51,10 @@ const AuthorType = new GraphQLObjectType({
         },
         books: {
             type: new GraphQLList(BookType),
-            resolve: xml => {
-                //this grabs data from parent element and extracts the ids.
+            resolve: (xml, args, context) => {
                 const ids = xml.GoodreadsResponse.author[0].books[0].book.map(elem=> elem.id[0]._)
-                console.log('Fetching Books!')
-                    return Promise.all(ids.map(id=> 
-                    //then for each id extract the book info and eventually passed into each BookType
-                    fetch(`https://www.goodreads.com/book/show${id}.xml?key=x7VoLKspIa5LO9bMmwxvIw`)
-                    .then(res=> res.text())
-                    .then(parseXML)
-                ))
-            }
-        },
+                return context. bookLoader.loadMany(ids)
+        }},
     })
 });
 
@@ -100,8 +70,7 @@ module.epxorts = new GraphQLSchema({
                 args: {
                     id: {type: GraphQLInt}
                 },
-                //function GQL used to fetch data for the author
-                resolve: (root, args) => fetchAuthor(args.id)
+                resolve: (root, args, context) => context.authorLoader.load(args.id)
             }
         })
     })
--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 const express = require('express')
 const graphqlHTTP = require('express-graphql')
-const schema = require('./schema')
 const app = express()
+const fetch = ('node-fetch')
+const schema = require('./schema')
+const DataLoader = require('dataloader')
+const util = require('util')
+const parseXML = util.promisify(require('xml2js').parseString)
 
-//SCHEMA
-//Contains types and all info on HOW to get data.
-//Where the magic happens.
+const fetchAuthor = id => {
+    fetch(`https://www.goodreads.com/author/show.xml?id=${id}&key=x7VoLKspIa5LO9bMmwxvIw`
+    )
+    .then(res=> res.text())
+    .then(parseXML)
+}
 
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: true
+const fetchBook = id => {
+    fetch(`https://www.goodreads.com/book/show${id}.xml?key=x7VoLKspIa5LO9bMmwxvIw`)
+        .then(res=> res.text())
+        .then(parseXML)
+}
+
+
+app.use('/graphql', graphqlHTTP(req=> {
+    //one param: a func (keys obj) returns a promise of array of values for keys
+    const authorLoader = new DataLoader(keys=>
+        Promise.all(keys.map(fetchAuthor)))
+    
+    const bookLoader = new DataLoader(keys=> 
+        Promise.all(keys.map(fetchBook)))
+    
+    return {
+        schema,
+        context: {
+            authorLoader,
+            bookLoader
+        },
+        graphiql: true
+    }
 }))
 
 
